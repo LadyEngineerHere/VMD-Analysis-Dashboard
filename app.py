6cbd802c040ecd082cb0d66a15973c15971f7b78
Amanda Gomez <132594395+LadyEngineerHere@users.noreply.github.com>
2023-09-12 11:30:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from credentials import DB_USERNAME, DB_PASSWORD  # Import database credentials
 
 # Initialize the Dash app with a Bootstrap theme
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 # Define a function to create image paths
 def get_image_path(brand):
