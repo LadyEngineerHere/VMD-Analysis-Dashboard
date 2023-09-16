177c08d8abb53c9e7d59a5165188da3bdd4f3473
Amanda Gomez <132594395+LadyEngineerHere@users.noreply.github.com>
2023-09-16 14:14:59 -0400

    Update app.py
    new profile, tables ,and settings page. new layout with Desktop and Mobile responsivness

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
+# Import necessary libraries
 import dash
+import dash_table
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 import pyodbc
 import dash_bootstrap_components as dbc
-import os
+from dash.exceptions import PreventUpdate
+from dash import dcc, html
+import os 
 
 # Initialize the Dash app with a Bootstrap theme
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-# Define a function to create image paths
+# Define a function to get image paths for different car brands
 def get_image_path(brand):
+    """
+    Given a car brand, return the path to the corresponding brand logo image.
+
+    Args:
+        brand (str): The car brand.
+
+    Returns:
+        str: The path to the brand logo image.
+    """
     if brand == 'Chevrolet':
         return '/assets/images/cheveroletlogo.png'
     elif brand == 'Ford':
@@ -26,7 +39,7 @@ def get_image_path(brand):
     else:
         return None
 
-# Define the list of cities and their corresponding latitude and longitude
+# Define data for cities and their corresponding latitude and longitude
 cities_data = {
     "City": ["Los Angeles", "New York", "Chicago", "Miami", "San Francisco", "Dallas", "Atlanta", "Phoenix", "Houston", "Seattle"],
     "Lat": [34.052235, 40.712776, 41.878113, 25.761681, 37.774929, 32.776665, 33.748997, 33.448376, 29.760427, 47.606209],
@@ -42,76 +55,246 @@ map_fig = px.scatter_geo(
     lat="Lat",
     lon="Lon",
     hover_name="City",
-    title="Location",
-    template='plotly_dark',  # Set dark background
+    title="City Locations",
+    template='plotly_dark',
 )
 
+# Establish a connection to the database
+server = "carserver1.database.windows.net"
+database = "cardb"
+driver = "{ODBC Driver 18 for SQL Server}"
+
+conn_str = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={DB_USERNAME};PWD={DB_PASSWORD}"
+connection = pyodbc.connect(conn_str)
+
+# Query all data from the database
+query = "SELECT * FROM CARSINFO"
+db_df = pd.read_sql(query, connection)
+
+# Close the database connection
+connection.close()
+
 # Define the layout of the app
-app.layout = html.Div(style={'backgroundColor': 'black', 'display': 'flex', 'height': '120vh'}, children=[
-    # Sidebar
-    html.Div(
-        className='sidebar',
-        children=[
-            html.H2("Vehicle Manufacturing Dataset", className="display-4", style={'color': '#333', 'font-size': '24px'}),
-            # Add image under "CARWORLD" title
-            html.Img(id='brand-logo', style={'width': '80%', 'margin-top': '20px', 'margin-bottom': '20px'}),
-            html.P("Make a selection from the dropdown menu", style={'color': '#555'}),
-            # Buttons for switching views
-            dbc.Button("Graphs", id="btn-graphs", color="danger", className="mb-2", style={'margin-right': '10px'}),
-            dbc.Button("Tables", id="btn-tables", color="danger", className="mb-2"),
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    # Sidebar
+                    html.Div(
+                        className='sidebar',
+                        children=[
+                            html.Div(
+                                children=[
+                                    # Centered content wrapper
+                                    html.Div(
+                                        children=[
+                                            html.H2("Vehicle Manufacturing Dataset", className="display-4", style={'color': '#555', 'font-size': '24px', 'text-align': 'center'}),
+                                            # Centered logo image
+                                            html.Div(
+                                                children=[
+                                                    html.Img(id='brand-logo', style={'width': '80%', 'margin': '0 auto', 'display': 'block'}),
+                                                ],
+                                                style={'text-align': 'center'},
+                                            ),
+                                            html.P("Select a car brand from the dropdown menu", style={'color': 'white', 'text-align': 'center'}),
+                                            # Navigation buttons
+                                            html.Div(
+                                                children=[
+                                                    dcc.Link("Graphs", href="/graphs", className="btn btn-danger mt-2", style={'width': '100%'}),
+                                                    dcc.Link("Tables", href="/tables", className="btn btn-danger mt-2", style={'width': '100%'}),
+                                                    dcc.Link("Profile", href="/profile", className="btn btn-primary mt-2", style={'width': '100%'}),  
+                                                    dcc.Link("Settings", href="/settings", className="btn btn-outline-primary mt-2", style={'width': '100%'}),  
+                                                ],
+                                                style={'padding': '1rem', 'background-color': '#101010'},
+                                            ),
+                                        ],
+                                        style={
+                                            'padding': '1rem',
+                                            'background-color': '#101010',
+                                            'minWidth': '200px',
+                                        }
+                                    ),
+                                    # Profile section
+                                    html.Div(
+                                        children=[
+                                            html.Img(src='/assets/images/Tomgomez.png', alt='avatar', className='rounded-circle img-fluid mx-auto d-block', style={'width': '150px'}),
+                                            html.H5('Tom Gomez', className='my-3', style={'text-align': 'center', 'color': 'white'}),
+                                            html.P('Intern Fall 2023', className='text-white mb-1', style={'text-align': 'center'}),
+                                            html.P('Greenville, SC', className='text-white mb-4', style={'text-align': 'center'}),
+                                            html.Div(
+                                                children=[
+                                                    dcc.Link("Profile", href="/profile", className="btn btn-primary"),
+                                                    dcc.Link("Settings", href="/settings", className="btn btn-outline-primary ms-1", style={'color': 'white'}),
+                                                ],
+                                                className="d-flex justify-content-center mb-2"
+                                            ),
+                                        ],
+                                        style={'padding': '1rem', 'background-color': '#101010', 'minWidth': '200px', 'margin-top': '20px'},
+                                    ),
+                                ],
+                            ),
+                        ],
+                        style={
+                            'flex': '0.1',
+                            'height': '100%',
+                        }
+                    ),
+                    md=3,
+                    sm=12,
+                ),
+                dbc.Col(
+                    # Main content
+                    html.Div(
+                        [
+                            dcc.Location(id='url', refresh=False),
+                            html.Div(id='page-content', className='content'),
+                        ],
+                        style={
+                            'flex': '0.9',
+                            'padding': '2rem',
+                            'background-color': 'black',
+                            'height': '100%',
+                            'color': 'white',
+                        },
+                    ),
+                    md=9,
+                    sm=12,
+                ),
+            ],
+            style={'background-color': 'black'}
+        ),
+    ],
+    fluid=True,
+)
+
+# Define the Graphs page content
+graphs_page = html.Div([
+    html.H1("Vehicle Manufacturing Data Analysis Dashboard", className="display-4", style={'color': 'white', 'font-size': '32px'}),
+    dcc.Dropdown(
+        id='brand-dropdown',
+        options=[
+            {'label': 'Chevrolet', 'value': 'Chevrolet'},
+            {'label': 'Ford', 'value': 'Ford'},
+            {'label': 'Honda', 'value': 'Honda'},
+            {'label': 'Hyundai', 'value': 'Hyundai'},
+            {'label': 'Toyota', 'value': 'Toyota'}
         ],
-        style={
-            'flex': '0.1',  # Make the sidebar thinner
-            'padding': '2rem',
-            'background-color': '#101010',
-            'height': '100%',
-        }
+        value='Toyota',
+        multi=False,
+        className='plotly_dark',
+        style={'backgroundColor': 'black', 'color': 'black'},
     ),
-    
-    # Main content
-    html.Div(
-        className='content',
-        children=[
-            html.H1("VMD Analysis Dashboard", className="display-4", style={'color': 'white', 'font-size': '32px'}),
-            dcc.Dropdown(
-                id='brand-dropdown',
-                # Options will be populated dynamically based on data from the database
-                options=[
-                    {'label': 'Chevrolet', 'value': 'Chevrolet'},
-                    {'label': 'Ford', 'value': 'Ford'},
-                    {'label': 'Honda', 'value': 'Honda'},
-                    {'label': 'Hyundai', 'value': 'Hyundai'},
-                    {'label': 'Toyota', 'value': 'Toyota'}
-                ],
-                value='Toyota',
-                multi=False,
-                className='plotly_dark',  # Set dropdown to 'plotly_dark' theme
-                style={'backgroundColor': 'black', 'color': '#808080'},  # Set background and text color
+    dbc.Row([
+        dbc.Col(
+            dbc.Container(
+                dcc.Graph(id='price-vs-mileage-scatter', config={'displayModeBar': True}),
+                style={'backgroundColor': '#111', 'border-radius': '15px', 'padding': '10px', 'margin-bottom': '20px'},
             ),
-            dbc.Row([  # Create a row to contain the charts side by side
-                dbc.Col(dcc.Graph(id='price-vs-mileage-scatter', style={'backgroundColor': 'black', 'height': '400px'}, config={'displayModeBar': True}), width=6),
-                dbc.Col(dcc.Graph(id='color-distribution-bar', style={'backgroundColor': 'black', 'height': '400px'}, config={'displayModeBar': True}), width=6),
-                dbc.Col(dcc.Graph(id='model-pie-chart', style={'backgroundColor': 'black', 'height': '400px'}, config={'displayModeBar': True}), width=6),
-                dbc.Col(dcc.Graph(id='location-map', figure=map_fig, style={'backgroundColor': 'black', 'height': '400px'}, config={'displayModeBar': True}), width=6),  # Map chart
-            ]),
-            # Include the "Graphs" and "Tables" views
-            html.Div(id='graphs-view', style={'display': 'block'}),  # Initially visible
-            html.Div(id='tables-view', style={'display': 'none'}),  # Initially hidden
-        ],
-        style={
-            'flex': '0.8',  # Make the content area wider
-            'padding': '2rem',
-            'background-color': 'black',  # Set background color to black
-            'height': '100%',
-            'color': 'white'  # Set text color to white
-        }
-    ),
+            md=6,
+            sm=12,
+        ),
+        dbc.Col(
+            dbc.Container(
+                dcc.Graph(id='color-distribution-bar', config={'displayModeBar': True}),
+                style={'backgroundColor': '#111', 'border-radius': '15px', 'padding': '10px', 'margin-bottom': '20px'},
+            ),
+            md=6,
+            sm=12,
+        ),
+    ]),
+    dbc.Row([
+        dbc.Col(
+            dbc.Container(
+                dcc.Graph(id='model-pie-chart', config={'displayModeBar': True}),
+                style={'backgroundColor': '#111', 'border-radius': '15px', 'padding': '10px', 'margin-bottom': '20px'},
+            ),
+            md=6,
+            sm=12,
+        ),
+        dbc.Col(
+            dbc.Container(
+                dcc.Graph(id='location-map', figure=map_fig, config={'displayModeBar': True}),
+                style={'backgroundColor': '#111', 'border-radius': '15px', 'padding': '10px', 'margin-bottom': '20px'},
+            ),
+            md=6,
+            sm=12,
+        ),
+    ]),
 ])
 
-# Define CSS styles for the chart container and dropdown menu
-app.css.append_css({
-    'external_url': 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-})
+# Define the Tables page content
+tables_page = html.Div([
+    html.H1("Tables", className="display-4", style={'color': 'white', 'font-size': '32px'}),
+    html.Div([
+        dash_table.DataTable(
+            columns=[{'name': col, 'id': col} for col in db_df.columns],
+            data=db_df.to_dict('records'),
+            style_table={'height': '500px', 'overflowY': 'auto'},
+            style_header={'backgroundColor': 'rgb(30, 30, 30)', 'color': 'white'},
+            style_cell={'backgroundColor': 'rgb(50, 50, 50)', 'color': 'white'},
+        )
+    ], style={'height': '500px'}),
+])
+
+# Define the Profile page content
+profile_page = html.Div([
+    html.H1("Profile", className="display-4", style={'color': 'white', 'font-size': '32px'}),
+    html.Div([
+        html.Img(src='/assets/images/Tomgomez.png', alt='avatar', className='rounded-circle img-fluid mx-auto d-block', style={'width': '150px'}),
+        html.H5('Tom Gomez', className='my-3', style={'text-align': 'center', 'color': 'white'}),
+        html.P('Intern Fall 2023', className='text-white mb-1', style={'text-align': 'center'}),
+        html.P('Greenville, SC', className='text-white mb-4', style={'text-align': 'center'}),
+        dbc.Row([
+            dbc.Col(html.H6("Full Name"), width=3),
+            dbc.Col(dcc.Input(type="text", className="form-control", value="Tom Gomez"), width=9),
+        ], className="mb-3"),
+        dbc.Row([
+            dbc.Col(html.H6("Email"), width=3),
+            dbc.Col(dcc.Input(type="text", className="form-control", value="TomGomez@example.com"), width=9),
+        ], className="mb-3"),
+        dbc.Row([
+            dbc.Col(html.H6("Phone"), width=3),
+            dbc.Col(dcc.Input(type="text", className="form-control", value="(239) 816-9029"), width=9),
+        ], className="mb-3"),
+        dbc.Row([
+            dbc.Col(html.H6("Postion"), width=3),
+            dbc.Col(dcc.Input(type="text", className="form-control", value="Intern Fall 2023"), width=9),
+        ], className="mb-3"),
+        dbc.Row([
+            dbc.Col(html.H6("Address"), width=3),
+            dbc.Col(dcc.Input(type="text", className="form-control", value="Greenville, SC"), width=9),
+        ], className="mb-3"),
+        dbc.Row([
+            dbc.Col(),  # Empty column to offset the button
+            dbc.Col(html.Button(type="button", className="btn btn-primary px-4", children="Save Changes"), width=9),
+        ]),
+    ], className="col-lg-8", style={'margin': '0 auto'}),
+])
+
+# Define the Settings page content (you can add more content here)
+settings_page = html.Div([
+    html.H1("Settings", className="display-4", style={'color': 'white', 'font-size': '32px'}),
+    html.P("This is the Settings page content.", style={'color': 'white'}),
+])
+
+# Callback to update the page content based on the URL
+@app.callback(
+    Output('page-content', 'children'),
+    Input('url', 'pathname')
+)
+def display_page(pathname):
+    if pathname == '/graphs':
+        return graphs_page
+    elif pathname == '/tables':
+        return tables_page
+    elif pathname == '/profile':
+        return profile_page
+    elif pathname == '/settings':
+        return settings_page
+    else:
+        # If the URL is invalid, return a 404 error
+        return '404 Page Not Found'
 
 # Callback to update the brand logo based on the selected brand
 @app.callback(
@@ -119,7 +302,6 @@ app.css.append_css({
     Input('brand-dropdown', 'value'),
 )
 def update_brand_logo(selected_brand):
-    # Update the brand logo
     return get_image_path(selected_brand)
 
 # Callback to update the scatter plot, color distribution bar chart, and model pie chart based on the selected brand
@@ -130,30 +312,24 @@ def update_brand_logo(selected_brand):
     [Input('brand-dropdown', 'value')],
 )
 def update_plots(selected_brand):
-    # Establish the connection to the database
-    DB_USERNAME = os.environ.get("DB_USERNAME")
-    DB_PASSWORD = os.environ.get("DB_PASSWORD")
     server = "carserver1.database.windows.net"
     database = "cardb"
     driver = "{ODBC Driver 18 for SQL Server}"
-    
+
     conn_str = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={DB_USERNAME};PWD={DB_PASSWORD}"
     connection = pyodbc.connect(conn_str)
-    
-    # Query data for the selected brand from the database
+
     query = f"SELECT * FROM CARSINFO WHERE Brand = '{selected_brand}'"
     filtered_df = pd.read_sql(query, connection)
-    
+
     connection.close()
-    
-    # Scatter plot
+
     scatter_fig = px.scatter(
         filtered_df, x='Mileage', y='Price', color='Color',
         title=f'Price vs Mileage for {selected_brand} Cars',
-        template='plotly_dark',  # Set dark background
+        template='plotly_dark',
     )
-    
-    # Color distribution bar chart
+
     color_distribution = filtered_df['Color'].value_counts().reset_index()
     color_distribution.columns = ['Color', 'Count']
     color_fig = px.bar(
@@ -161,41 +337,19 @@ def update_plots(selected_brand):
         title=f'Color Distribution for {selected_brand} Cars',
         color='Color',
         color_discrete_sequence=px.colors.qualitative.Set1,
-        template='plotly_dark',  # Set dark background
+        template='plotly_dark',
     )
-    
-    # Model pie chart
+
     model_distribution = filtered_df['Model'].value_counts().reset_index()
     model_distribution.columns = ['Model', 'Count']
     model_pie_fig = px.pie(
         model_distribution, names='Model', values='Count',
         title=f'Model Distribution for {selected_brand} Cars',
-        template='plotly_dark',  # Set dark background
+        template='plotly_dark',
     )
-    
+
     return scatter_fig, color_fig, model_pie_fig
 
-# Callback to toggle between "Graphs" and "Tables" views
-@app.callback(
-    [Output('graphs-view', 'style'),
-     Output('tables-view', 'style')],
-    [Input('btn-graphs', 'n_clicks'),
-     Input('btn-tables', 'n_clicks')]
-)
-def toggle_views(btn_graphs, btn_tables):
-    ctx = dash.callback_context
-    if not ctx.triggered:
-        return {'display': 'block'}, {'display': 'none'}
-    
-    button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-    
-    if button_id == 'btn-graphs':
-        return {'display': 'block'}, {'display': 'none'}
-    elif button_id == 'btn-tables':
-        return {'display': 'none'}, {'display': 'block'}
-    
-    return {'display': 'block'}, {'display': 'none'}
-
 # Run the app
-if __name__ == '__main__': 
-    app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=False)
